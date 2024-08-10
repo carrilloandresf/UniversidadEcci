9e830c152221fcfbf6c8b62b055bb43740fb648a
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-08-10 00:48:55 -0500

    Cargue inicia Demos

---
+++ AlgoritmosDeRobotica/Demos/LED.py
@@ -0,0 +1,16 @@
+"""
+Es un LED que se enciende y se apaga con un tiempo de espera de 0.5 segundos.
+Para encender el LED se debe encender el GPIO 17.
+Para apagar el LED se debe apagar el GPIO 17.
+"""
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(0.5)
+    led.off()
+    sleep(0.5)
--- AlgoritmosDeRobotica/Taller1/Punto4.py
+++ AlgoritmosDeRobotica/Taller1/Punto4.py
@@ -10,6 +10,35 @@ R0 es la resistencia a la temperatura de referencia T0 (para el PT100, R0 es 100
 α es el coeficiente de temperatura del platino (para el PT100, aproximadamente 0.00385 por °C).
 T es la temperatura en grados Celsius.
 T0 es la temperatura de referencia (0°C).
+
+
+adicionar
+Calculadora RTD
+ 
+Calculadora de resistencia para RTD de platino de conformidad con IEC 60751
+Cómo utilizar la calculadora de resistencia RTD:
+
+Introduzca la resistencia RTD a 0°C. (La mayoría de los RTD tienen una resistencia de 100 ohms a 0°C).
+Introduzca la temperatura RTD en °C.
+Lea la resistencia RTD
+ 
+
+Calculadora de resistencia para RTD de platino de conformidad con IEC 60751:
+
+ 
+
+Introduzca la resistencia RTD a 0°C
+Introduzca la temperatura RTD en °C.
+Resistencia RTD (ohms)
+ 
+
+Ecuaciones de Callendar-Van Dusen:
+
+
+Temperatura RTD ≥ 0°C: Rt = R0(1+At+Bt2)
+
+
+Temperatura RTD < 0°C: Rt = R0[1+At+Bt2+C(t-100)t3]
 """
 
 def resistencia_pt100(temperatura):
