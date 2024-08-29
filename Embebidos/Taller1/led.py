9fbb6fb60951c7fb4d8b6ac8fe25767a35fa3f1f
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-08-28 21:46:59 -0500

    actualizacion embebidos

--- Embebidos/Taller1/led.py
+++ Embebidos/Taller1/led.py
@@ -1,29 +1,21 @@
-import gpiod
+import RPi.GPIO as GPIO
 import time
 
-# Definir el chip GPIO, usualmente /dev/gpiochip0
-chip = gpiod.Chip('gpiochip0')
+# Configurar la numeración de los pines
+GPIO.setmode(GPIO.BCM)
 
 # Lista de pines GPIO a utilizar (del 2 al 27)
 pins = list(range(2, 28))
 
-# Configurar las líneas GPIO como salidas
-lines = chip.get_lines(pins)
-lines.request(consumer="led_control", type=gpiod.LINE_REQ_DIR_OUT)
+# Configurar cada pin como salida
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
 
 # Encender los LEDs uno por uno con un retardo de 0.5 segundos
 try:
-    for i in range(len(pins)):
-        # Encender el LED correspondiente
-        values = [1 if j == i else 0 for j in range(len(pins))]
-        lines.set_values(values)
-        time.sleep(0.5)  # Esperar 0.5 segundos
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)  # Encender el LED
+        time.sleep(0.5)              # Esperar 0.5 segundos
 
-        # Apagar el LED correspondiente
-        values[i] = 0
-        lines.set_values(values)
-        time.sleep(0.5)  # Esperar 0.5 segundos
-
-finally:
-    # Liberar las líneas GPIO
-    lines.release()
+except KeyboardInterrupt:
+   GPIO.cleanup()
