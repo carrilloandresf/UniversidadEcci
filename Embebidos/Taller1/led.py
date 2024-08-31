9866ff5daf77f3a0e8dcbbc919d7e060a17726b0
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-08-31 10:54:18 -0500

    homologacion codigo leds

--- Embebidos/Taller1/led.py
+++ Embebidos/Taller1/led.py
@@ -1,21 +1,29 @@
-import RPi.GPIO as GPIO
+import gpiod
 import time
-
-# Configurar la numeración de los pines
-GPIO.setmode(GPIO.BCM)
 
 # Lista de pines GPIO a utilizar (del 2 al 27)
 pins = list(range(2, 28))
 
-# Configurar cada pin como salida
-for pin in pins:
-    GPIO.setup(pin, GPIO.OUT)
+# Configurar el chip GPIO
+chip = gpiod.Chip('gpiochip4')
+
+# Crear una lista de líneas para los pines
+lines = [chip.get_line(pin) for pin in pins]
+
+# Configurar cada línea como salida
+for line in lines:
+    line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
 
 # Encender los LEDs uno por uno con un retardo de 0.5 segundos
 try:
-    for pin in pins:
-        GPIO.output(pin, GPIO.HIGH)  # Encender el LED
-        time.sleep(0.5)              # Esperar 0.5 segundos
+    for line in lines:
+        line.set_value(1)  # Encender el LED
+        time.sleep(0.5)    # Esperar 0.5 segundos
 
 except KeyboardInterrupt:
-   GPIO.cleanup()
+    pass
+
+finally:
+    # Liberar las líneas al final
+    for line in lines:
+        line.release()
