818d20f2ac3f4eecaca763aca10f1ea5871e1401
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-09-24 16:53:18 -0500

    ajuste dimensiones

--- AlgoritmosDeRobotica/Taller2/punto4.py
+++ AlgoritmosDeRobotica/Taller2/punto4.py
@@ -11,13 +11,41 @@ import numpy as np
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(789, 715)
+        MainWindow.resize(900, 750)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        # Layout de sliders
+        # Redimensionar y colocar el gráfico
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(100, 30, 700, 300))  # Redimensionado
+        self.widget.setObjectName("widget")
+
+        # Layout para el gráfico
+        self.graph_layout = QVBoxLayout(self.widget)
+        self.figure, self.ax = plt.subplots()
+        self.canvas = FigureCanvas(self.figure)
+        self.graph_layout.addWidget(self.canvas)
+
+        # Logo de la universidad
+        self.label_logo = QtWidgets.QLabel(self.centralwidget)
+        self.label_logo.setGeometry(QtCore.QRect(50, 340, 150, 150))
+        self.label_logo.setText("")
+        self.label_logo.setPixmap(QtGui.QPixmap("../../src/img/logo.png"))  # Reemplaza por la ruta de tu logo
+        self.label_logo.setScaledContents(True)
+        self.label_logo.setObjectName("label_logo")
+
+        # Nombres del equipo
+        self.label_equipo = QtWidgets.QLabel(self.centralwidget)
+        self.label_equipo.setGeometry(QtCore.QRect(220, 340, 300, 100))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setPointSize(10)  # Ajuste del tamaño de fuente
+        self.label_equipo.setFont(font)
+        self.label_equipo.setObjectName("label_equipo")
+
+        # Sliders para voltaje, capacitancia y resistencia
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(-30, 350, 831, 151))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(100, 500, 700, 150))  # Ajustado para mayor espacio
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -47,57 +75,30 @@ class Ui_MainWindow(object):
         self.slider_res.setObjectName("slider_res")
         self.horizontalLayout.addWidget(self.slider_res)
 
-        # Labels para mostrar valores de sliders
+        # Labels para mostrar los valores de los sliders
         self.label_val_volt = QtWidgets.QLabel(self.centralwidget)
-        self.label_val_volt.setGeometry(QtCore.QRect(60, 520, 120, 20))
+        self.label_val_volt.setGeometry(QtCore.QRect(130, 670, 120, 20))
         self.label_val_volt.setObjectName("label_val_volt")
 
         self.label_val_cap = QtWidgets.QLabel(self.centralwidget)
-        self.label_val_cap.setGeometry(QtCore.QRect(240, 520, 120, 20))
+        self.label_val_cap.setGeometry(QtCore.QRect(300, 670, 120, 20))
         self.label_val_cap.setObjectName("label_val_cap")
 
         self.label_val_res = QtWidgets.QLabel(self.centralwidget)
-        self.label_val_res.setGeometry(QtCore.QRect(420, 520, 120, 20))
+        self.label_val_res.setGeometry(QtCore.QRect(500, 670, 120, 20))
         self.label_val_res.setObjectName("label_val_res")
 
-        # Label para la fórmula utilizada
+        # Label para la fórmula
         self.label_formula = QtWidgets.QLabel(self.centralwidget)
-        self.label_formula.setGeometry(QtCore.QRect(60, 560, 600, 40))
+        self.label_formula.setGeometry(QtCore.QRect(130, 700, 600, 40))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_formula.setFont(font)
         self.label_formula.setObjectName("label_formula")
 
-        # Sección para el logo
-        self.label_logo = QtWidgets.QLabel(self.centralwidget)
-        self.label_logo.setGeometry(QtCore.QRect(60, 490, 131, 41))
-        self.label_logo.setText("")
-        self.label_logo.setPixmap(QtGui.QPixmap("logo.png"))  # Asegúrate de poner la ruta correcta del logo
-        self.label_logo.setScaledContents(True)
-        self.label_logo.setObjectName("label_logo")
-
-        # Sección para los nombres del equipo
-        self.label_equipo = QtWidgets.QLabel(self.centralwidget)
-        self.label_equipo.setGeometry(QtCore.QRect(60, 390, 221, 81))
-        font = QtGui.QFont()
-        font.setBold(True)
-        self.label_equipo.setFont(font)
-        self.label_equipo.setObjectName("label_equipo")
-
-        # Widget para el gráfico de matplotlib
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(220, 30, 341, 221))
-        self.widget.setObjectName("widget")
-
-        # Layout para el gráfico
-        self.graph_layout = QVBoxLayout(self.widget)
-        self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.figure)
-        self.graph_layout.addWidget(self.canvas)
-
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 789, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 900, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -107,12 +108,12 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        # Conectar los sliders a la función que actualiza el gráfico y los valores de los labels
+        # Conectar sliders con el gráfico
         self.slider_volt.valueChanged.connect(self.update_graph)
         self.slider_cap.valueChanged.connect(self.update_graph)
         self.slider_res.valueChanged.connect(self.update_graph)
 
-        self.update_graph()  # Llamada inicial para dibujar el gráfico con valores predeterminados
+        self.update_graph()  # Llamada inicial para dibujar el gráfico
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -124,7 +125,7 @@ class Ui_MainWindow(object):
         self.label_equipo.setText(_translate("MainWindow", "<html><body><p>Andrés Felipe Carrillo Rodríguez<br>Daniela Rodríguez Pelaez<br>Jeisson Gutierrez Sanchez<br>William Alejandro Fernandez Pinzón</p><p>Ingeniería Mecatrónica<br>Electiva de Robótica<br>2024 - II</p></body></html>"))
 
     def update_graph(self):
-        # Obtener valores de los sliders
+        # Obtener valores de sliders
         V = self.slider_volt.value()  # Voltaje
         C = self.slider_cap.value() * 1e-6  # Capacitancia en Farads
         R = self.slider_res.value()  # Resistencia
