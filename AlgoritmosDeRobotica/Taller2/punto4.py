64e085a22d07b29098cff2b4ed2885edeea4c88b
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-09-26 18:16:16 -0500

    Actualizacion punto4

--- AlgoritmosDeRobotica/Taller2/punto4.py
+++ AlgoritmosDeRobotica/Taller2/punto4.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QVBoxLayout
-import matplotlib
-matplotlib.use('Qt5Agg')  # Utilizar el backend para PyQt5
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QGraphicsScene
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -15,20 +13,9 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        # Redimensionar y colocar el gráfico
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(100, 30, 600, 250))  # Redimensionado y centrado
-        self.widget.setObjectName("widget")
-
-        # Layout para el gráfico
-        self.graph_layout = QVBoxLayout(self.widget)
-        self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.figure)
-        self.graph_layout.addWidget(self.canvas)
-
         # Logo de la universidad
         self.label_logo = QtWidgets.QLabel(self.centralwidget)
-        self.label_logo.setGeometry(QtCore.QRect(50, 300, 100, 100))  # Ajustado tamaño del logo
+        self.label_logo.setGeometry(QtCore.QRect(50, 300, 100, 100))
         self.label_logo.setText("")
         self.label_logo.setPixmap(QtGui.QPixmap("../../src/img/logo.png"))  # Ruta actualizada para el logo
         self.label_logo.setScaledContents(True)
@@ -36,61 +23,58 @@ class Ui_MainWindow(object):
 
         # Nombres del equipo
         self.label_equipo = QtWidgets.QLabel(self.centralwidget)
-        self.label_equipo.setGeometry(QtCore.QRect(170, 300, 300, 100))  # Ajuste de la posición
+        self.label_equipo.setGeometry(QtCore.QRect(170, 300, 300, 100))
         font = QtGui.QFont()
         font.setBold(True)
-        font.setPointSize(9)  # Ajuste del tamaño de fuente
+        font.setPointSize(9)
         self.label_equipo.setFont(font)
         self.label_equipo.setObjectName("label_equipo")
 
+        # Gráfica usando QGraphicsView
+        self.Grafica = QtWidgets.QGraphicsView(self.centralwidget)
+        self.Grafica.setGeometry(QtCore.QRect(100, 30, 600, 250))
+        self.Grafica.setObjectName("Grafica")
+        self.scene = QGraphicsScene()
+        self.Grafica.setScene(self.scene)
+
         # Sliders para voltaje, capacitancia y resistencia
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(100, 450, 600, 120))  # Ajustado para mayor espacio
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.Resistencia = QtWidgets.QSlider(self.centralwidget)
+        self.Resistencia.setGeometry(QtCore.QRect(130, 450, 150, 22))
+        self.Resistencia.setMinimum(1)
+        self.Resistencia.setMaximum(1000)
+        self.Resistencia.setOrientation(QtCore.Qt.Horizontal)
+        self.Resistencia.setObjectName("Resistencia")
 
-        # Slider para el voltaje
-        self.slider_volt = QtWidgets.QSlider(self.horizontalLayoutWidget)
-        self.slider_volt.setMinimum(1)
-        self.slider_volt.setMaximum(20)
-        self.slider_volt.setOrientation(QtCore.Qt.Vertical)
-        self.slider_volt.setObjectName("slider_volt")
-        self.horizontalLayout.addWidget(self.slider_volt)
+        self.Voltaje = QtWidgets.QSlider(self.centralwidget)
+        self.Voltaje.setGeometry(QtCore.QRect(350, 450, 150, 22))
+        self.Voltaje.setMinimum(1)
+        self.Voltaje.setMaximum(20)
+        self.Voltaje.setOrientation(QtCore.Qt.Horizontal)
+        self.Voltaje.setObjectName("Voltaje")
 
-        # Slider para la capacitancia
-        self.slider_cap = QtWidgets.QSlider(self.horizontalLayoutWidget)
-        self.slider_cap.setMinimum(1)
-        self.slider_cap.setMaximum(100)
-        self.slider_cap.setOrientation(QtCore.Qt.Vertical)
-        self.slider_cap.setObjectName("slider_cap")
-        self.horizontalLayout.addWidget(self.slider_cap)
-
-        # Slider para la resistencia
-        self.slider_res = QtWidgets.QSlider(self.horizontalLayoutWidget)
-        self.slider_res.setMinimum(1)
-        self.slider_res.setMaximum(1000)
-        self.slider_res.setOrientation(QtCore.Qt.Vertical)
-        self.slider_res.setObjectName("slider_res")
-        self.horizontalLayout.addWidget(self.slider_res)
+        self.Capacitancia = QtWidgets.QSlider(self.centralwidget)
+        self.Capacitancia.setGeometry(QtCore.QRect(570, 450, 150, 22))
+        self.Capacitancia.setMinimum(1)
+        self.Capacitancia.setMaximum(100)
+        self.Capacitancia.setOrientation(QtCore.Qt.Horizontal)
+        self.Capacitancia.setObjectName("Capacitancia")
 
         # Labels para mostrar los valores de los sliders
         self.label_val_volt = QtWidgets.QLabel(self.centralwidget)
-        self.label_val_volt.setGeometry(QtCore.QRect(130, 570, 120, 20))
+        self.label_val_volt.setGeometry(QtCore.QRect(350, 480, 120, 20))
         self.label_val_volt.setObjectName("label_val_volt")
 
         self.label_val_cap = QtWidgets.QLabel(self.centralwidget)
-        self.label_val_cap.setGeometry(QtCore.QRect(300, 570, 120, 20))
+        self.label_val_cap.setGeometry(QtCore.QRect(570, 480, 120, 20))
         self.label_val_cap.setObjectName("label_val_cap")
 
         self.label_val_res = QtWidgets.QLabel(self.centralwidget)
-        self.label_val_res.setGeometry(QtCore.QRect(500, 570, 120, 20))
+        self.label_val_res.setGeometry(QtCore.QRect(130, 480, 120, 20))
         self.label_val_res.setObjectName("label_val_res")
 
         # Label para la fórmula
         self.label_formula = QtWidgets.QLabel(self.centralwidget)
-        self.label_formula.setGeometry(QtCore.QRect(130, 600, 600, 30))
+        self.label_formula.setGeometry(QtCore.QRect(130, 510, 600, 30))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.label_formula.setFont(font)
@@ -108,12 +92,12 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        # Conectar sliders con el gráfico
-        self.slider_volt.valueChanged.connect(self.update_graph)
-        self.slider_cap.valueChanged.connect(self.update_graph)
-        self.slider_res.valueChanged.connect(self.update_graph)
+        # Conectar sliders con la función de actualización de la gráfica
+        self.Resistencia.valueChanged.connect(self.datos)
+        self.Capacitancia.valueChanged.connect(self.datos)
+        self.Voltaje.valueChanged.connect(self.datos)
 
-        self.update_graph()  # Llamada inicial para dibujar el gráfico
+        self.configurar()  # Llamada inicial para configurar la gráfica
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -124,40 +108,52 @@ class Ui_MainWindow(object):
         self.label_formula.setText(_translate("MainWindow", "Fórmula (Carga y Descarga): Vc = V * (1 - exp(-t / (R*C))) para carga, Vc = V * exp(-t / (R*C)) para descarga"))
         self.label_equipo.setText(_translate("MainWindow", "<html><body><p>Andrés Felipe Carrillo Rodríguez<br>Daniela Rodríguez Pelaez<br>Jeisson Gutierrez Sanchez<br>William Alejandro Fernandez Pinzón</p><p>Ingeniería Mecatrónica<br>Electiva de Robótica<br>2024 - II</p></body></html>"))
 
-    def update_graph(self):
+    def configurar(self):
+        # Configurar la figura para la gráfica
+        self.figure = Figure(figsize=(2.5, 1.5))
+        self.canvas = FigureCanvas(self.figure)
+        self.scene.addWidget(self.canvas)
+        self.ax = self.figure.add_subplot(111)
+        self.ax.set_xlabel("Tiempo (s)")
+        self.ax.set_ylabel("Voltaje (V)")
+        self.datos()
+
+    def datos(self):
         # Obtener valores de sliders
-        V = self.slider_volt.value()  # Voltaje
-        C = self.slider_cap.value() * 1e-6  # Capacitancia en Farads
-        R = self.slider_res.value()  # Resistencia
+        R = self.Resistencia.value()
+        C = self.Capacitancia.value() / 10000
+        V = self.Voltaje.value()
 
         # Actualizar los labels con los valores actuales
         self.label_val_volt.setText(f"Voltaje (V): {V}")
-        self.label_val_cap.setText(f"Capacitancia (uF): {self.slider_cap.value()}")
+        self.label_val_cap.setText(f"Capacitancia (uF): {self.Capacitancia.value()}")
         self.label_val_res.setText(f"Resistencia (Ohms): {R}")
 
         # Tiempo para la graficación
-        t_carga = np.linspace(0, 5, 500)  # Tiempo para carga
-        t_descarga = np.linspace(5, 10, 500)  # Tiempo para descarga
+        t = np.linspace(0, 1, 4000)
 
         # Calcular la carga y descarga del condensador
-        tau = R * C
-        Vc_carga = V * (1 - np.exp(-t_carga / tau))  # Ecuación de carga
-        Vc_descarga = V * np.exp(-(t_descarga - 5) / tau)  # Ecuación de descarga, empieza desde t=5
+        Carga = self.carga(V, R, C, t)
+        Descarga = self.descarga(V, R, C, t)
 
         # Limpiar el gráfico anterior
         self.ax.clear()
 
         # Graficar la carga y descarga del condensador
-        self.ax.plot(t_carga, Vc_carga, label="Carga del condensador")
-        self.ax.plot(t_descarga, Vc_descarga, label="Descarga del condensador", linestyle='--')
-        self.ax.set_title('Carga y Descarga del Condensador')
-        self.ax.set_xlabel('Tiempo (s)')
-        self.ax.set_ylabel('Voltaje (V)')
+        self.ax.plot(t, Carga, label="Carga")
+        self.ax.plot(t, Descarga, label="Descarga", linestyle="--")
+        self.ax.set_xlabel("Tiempo (s)")
+        self.ax.set_ylabel("Voltaje (V)")
         self.ax.legend()
-        self.ax.grid(True)
 
         # Refrescar el canvas del gráfico
         self.canvas.draw()
+
+    def carga(self, V, R, C, t):
+        return V * (1 - np.exp(-t / (R * C)))
+
+    def descarga(self, V, R, C, t):
+        return V * np.exp(-t / (R * C))
 
 if __name__ == "__main__":
     import sys
