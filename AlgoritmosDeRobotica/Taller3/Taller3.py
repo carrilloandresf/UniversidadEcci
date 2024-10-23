2dbac8047801bfd4b1e7886d67361454442aedbf
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-10-23 13:58:16 -0500

    Actualizacion de codigo

--- AlgoritmosDeRobotica/Taller3/Taller3.py
+++ AlgoritmosDeRobotica/Taller3/Taller3.py
@@ -35,8 +35,7 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(860, 640)
-
-        # Definición de todos los widgets (tomados de Taller3.ui)
+        # Aquí continúa la configuración de la interfaz gráfica
         self.label_2 = QtWidgets.QLabel(Dialog)
         self.label_2.setGeometry(QtCore.QRect(40, 440, 261, 181))
         font = QtGui.QFont()
@@ -44,123 +43,112 @@ class Ui_Dialog(object):
         self.label_2.setFont(font)
         self.label_2.setScaledContents(True)
         self.label_2.setObjectName("label_2")
-
         self.label_3 = QtWidgets.QLabel(Dialog)
         self.label_3.setGeometry(QtCore.QRect(450, 400, 271, 161))
         self.label_3.setText("")
         self.label_3.setPixmap(QtGui.QPixmap("../Taller2/logoUniversidadEcci.jpg"))
         self.label_3.setScaledContents(True)
+        self.label_3.setWordWrap(False)
         self.label_3.setObjectName("label_3")
-
         self.label = QtWidgets.QLabel(Dialog)
         self.label.setGeometry(QtCore.QRect(270, 10, 291, 41))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.label.setFont(font)
         self.label.setObjectName("label")
-
         self.label_4 = QtWidgets.QLabel(Dialog)
         self.label_4.setGeometry(QtCore.QRect(20, 60, 291, 41))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
-
         self.label_5 = QtWidgets.QLabel(Dialog)
         self.label_5.setGeometry(QtCore.QRect(150, 60, 291, 41))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.label_5.setFont(font)
         self.label_5.setObjectName("label_5")
-
         self.label_6 = QtWidgets.QLabel(Dialog)
         self.label_6.setGeometry(QtCore.QRect(30, 130, 291, 41))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.label_6.setFont(font)
         self.label_6.setObjectName("label_6")
-
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
         self.lineEdit.setGeometry(QtCore.QRect(20, 90, 113, 22))
         self.lineEdit.setObjectName("lineEdit")
-
         self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
         self.lineEdit_2.setGeometry(QtCore.QRect(150, 90, 113, 22))
         self.lineEdit_2.setObjectName("lineEdit_2")
-
         self.label_7 = QtWidgets.QLabel(Dialog)
         self.label_7.setGeometry(QtCore.QRect(150, 130, 81, 41))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.label_7.setFont(font)
         self.label_7.setObjectName("label_7")
-
         self.label_8 = QtWidgets.QLabel(Dialog)
         self.label_8.setGeometry(QtCore.QRect(280, 130, 81, 41))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.label_8.setFont(font)
         self.label_8.setObjectName("label_8")
-
+        self.graphicsView = QtWidgets.QGraphicsView(Dialog)
+        self.graphicsView.setGeometry(QtCore.QRect(450, 80, 256, 192))
+        self.graphicsView.setObjectName("graphicsView")
         self.pushButton = QtWidgets.QPushButton(Dialog)
         self.pushButton.setGeometry(QtCore.QRect(20, 180, 80, 22))
         self.pushButton.setObjectName("pushButton")
-
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(110, 210, 80, 22))
         self.pushButton_2.setObjectName("pushButton_2")
-
         self.pushButton_3 = QtWidgets.QPushButton(Dialog)
         self.pushButton_3.setGeometry(QtCore.QRect(200, 210, 80, 22))
         self.pushButton_3.setObjectName("pushButton_3")
-
         self.pushButton_4 = QtWidgets.QPushButton(Dialog)
         self.pushButton_4.setGeometry(QtCore.QRect(20, 210, 80, 22))
         self.pushButton_4.setObjectName("pushButton_4")
-
         self.pushButton_5 = QtWidgets.QPushButton(Dialog)
         self.pushButton_5.setGeometry(QtCore.QRect(290, 210, 80, 22))
         self.pushButton_5.setObjectName("pushButton_5")
-
         self.pushButton_6 = QtWidgets.QPushButton(Dialog)
         self.pushButton_6.setGeometry(QtCore.QRect(290, 240, 80, 22))
         self.pushButton_6.setObjectName("pushButton_6")
-
         self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
         self.lineEdit_3.setGeometry(QtCore.QRect(20, 240, 261, 22))
         self.lineEdit_3.setObjectName("lineEdit_3")
-
         self.pushButton_7 = QtWidgets.QPushButton(Dialog)
         self.pushButton_7.setGeometry(QtCore.QRect(20, 280, 80, 22))
         self.pushButton_7.setObjectName("pushButton_7")
-
         self.pushButton_8 = QtWidgets.QPushButton(Dialog)
         self.pushButton_8.setGeometry(QtCore.QRect(110, 280, 80, 22))
         self.pushButton_8.setObjectName("pushButton_8")
-
         self.pushButton_9 = QtWidgets.QPushButton(Dialog)
         self.pushButton_9.setGeometry(QtCore.QRect(200, 280, 80, 22))
         self.pushButton_9.setObjectName("pushButton_9")
-
         self.pushButton_10 = QtWidgets.QPushButton(Dialog)
         self.pushButton_10.setGeometry(QtCore.QRect(290, 280, 80, 22))
         self.pushButton_10.setObjectName("pushButton_10")
-
         self.pushButton_11 = QtWidgets.QPushButton(Dialog)
         self.pushButton_11.setGeometry(QtCore.QRect(280, 90, 80, 22))
         self.pushButton_11.setObjectName("pushButton_11")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
         # Conexiones de botones
         self.pushButton_11.clicked.connect(self.move_to_position)
         self.pushButton.clicked.connect(self.draw_yin_yang)
 
+        # Asigna la función a los botones de nombres
         self.pushButton_2.clicked.connect(lambda: self.write_name("Felipe"))
         self.pushButton_3.clicked.connect(lambda: self.write_name("Jeisson"))
         self.pushButton_4.clicked.connect(lambda: self.write_name("Daniela"))
         self.pushButton_5.clicked.connect(lambda: self.write_name("William"))
 
+        # Botón para escribir palabra ingresada en lineEdit_3
         self.pushButton_6.clicked.connect(self.write_custom_word)
 
+        # Botones para logos (Ejemplo)
         self.pushButton_7.clicked.connect(lambda: self.draw_logo("Puma"))
         self.pushButton_8.clicked.connect(lambda: self.draw_logo("Toyota"))
         self.pushButton_9.clicked.connect(lambda: self.draw_logo("Apple"))
@@ -191,6 +179,7 @@ class Ui_Dialog(object):
     def move_to_position(self):
         x = float(self.lineEdit.text())
         y = float(self.lineEdit_2.text())
+        # Calcular ángulos inversos (IK) para alcanzar la posición (x, y)
         theta1, theta2 = self.inverse_kinematics(x, y)
         self.set_servo_angle(servo1, theta1)
         self.set_servo_angle(servo2, theta2)
@@ -198,6 +187,7 @@ class Ui_Dialog(object):
         self.label_8.setText(f"{theta2:.2f}")
 
     def inverse_kinematics(self, x, y):
+        # Simulación del cálculo de ángulos inversos para el robot SCARA
         d1 = 1  # Longitud del primer brazo
         d2 = 1  # Longitud del segundo brazo
         theta2 = np.arccos((x**2 + y**2 - d1**2 - d2**2) / (2 * d1 * d2))
@@ -213,17 +203,22 @@ class Ui_Dialog(object):
             self.label_8.setText(f"{s2:.2f}")
 
     def write_name(self, name):
+        # Lógica de escritura de nombre con el efector final
+        # Podrías usar un método básico para cada letra en el nombre
         self.set_servo_angle(servo1, 45)
         self.set_servo_angle(servo2, 45)
+        # (Completar la lógica de escritura)
 
     def write_custom_word(self):
         word = self.lineEdit_3.text()
         self.write_name(word)
 
     def draw_logo(self, logo_name):
+        # Lógica para trazar los logos específicos
         self.set_servo_angle(servo1, 0)
         self.set_servo_angle(servo2, 0)
-        time.sleep(2)
+        time.sleep(2)  # Espera para que el usuario coloque el papel
+        # (Implementar lógica para trazar el logo paso a paso)
 
 if __name__ == "__main__":
     import sys
