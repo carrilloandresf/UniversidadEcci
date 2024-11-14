1828694ee930b3330b6ddde9bf380a005b255c5c
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2024-11-14 14:04:43 -0500

    ajuste rollback2

--- AlgoritmosDeRobotica/ProyectoFinal/proyectoFinal.py
+++ AlgoritmosDeRobotica/ProyectoFinal/proyectoFinal.py
@@ -232,6 +232,7 @@ class Ui_MainWindow(object):
         if hasattr(self, 'simulation') and self.simulation:
             # Llamar a `draw_idle` para actualizar la visualización sin pausar el bucle de eventos
             self.simulation.fig.canvas.draw_idle()
+            self.simulation.fig.canvas.flush_events()
 
 
     def move_servos_smoothly(self, servo_motor, target_angle, joint_index=None, steps=20, delay=0.01):
