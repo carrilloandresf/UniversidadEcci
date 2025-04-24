144c39391eff347f889975a8d0b25bb39595a397
Andrés Felipe Carrillo Rodríguez <anfelcaro@hotmail.com>
2025-04-23 20:17:00 -0500

    Ajuste de codigo

--- DisenoMecatronico/ProyectoFinal.py
+++ DisenoMecatronico/ProyectoFinal.py
@@ -104,7 +104,7 @@ try:
         # Si el CNY70 detecta un vaso (el valor es 0)
         elif estado_cny70:
             # Mover motor 2 (banda) para llenar el vaso
-            mover_motor_paso_a_paso_1(5)
+            mover_motor_paso_a_paso_1(1)
             imprimir_sobre_linea("Llenando...                                                              ")
             mover_motor_paso_a_paso_2(10)  # Mantiene el motor girando indefinidamente
 
